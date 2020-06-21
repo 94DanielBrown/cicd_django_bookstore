7a84961a13f22a4c6351cdd051a36410d9c6382f
Daniel Brown <94daniel.brown@gmail.com>
2020-06-21 18:39:47 +0000

    Update settings.py

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -102,8 +102,8 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': '172.21.0.2',
-        'PORT': 5432
+        'HOST': '172.25.0.2',
+        'PORT': 5434
     }
 }
 
